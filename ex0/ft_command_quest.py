0ab69eaafb376a12bf4e3cc22ed44faa6a183b8a
Biditchoun <sawijnbe@shi-r4-p7.s19.be>
2026-03-30 20:23:06 +0200

    ex0 done

---
+++ ex0/ft_command_quest.py
@@ -0,0 +1,16 @@
+import sys
+
+
+if __name__ == "__main__":
+    print("=== Command Quest ===")
+    print(f"Program name: {sys.argv[0]}")
+    argc = len(sys.argv)
+    if (argc == 1):
+        print("No arguments provided!")
+    else:
+        print(f"Arguments received: {argc - 1}")
+    i = 1
+    while (i < argc):
+        print(f"Argument {i}: {sys.argv[i]}")
+        i += 1
+    print(f"Total arguments: {argc}")
